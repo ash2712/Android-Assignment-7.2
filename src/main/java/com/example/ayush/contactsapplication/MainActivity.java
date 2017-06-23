c12d1ec271ac088b41dd0c40f8431a27a74a789b
ash2712 <spellguru@gmail.com>
2017-06-23 22:15:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/ayush/contactsapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ayush.contactsapplication;
+
+import android.content.Intent;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.provider.Contacts.CONTENT_URI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bt1 = (Button) findViewById(R.id.button);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+}
